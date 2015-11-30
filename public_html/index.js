835c1cab816efe9a3c753999e1fb35523d738f6f
s <git@tnu.jp>
2015-11-30 11:05:43 +0900

    failed

--- public_html/index.js
+++ public_html/index.js
@@ -1,4 +1,8 @@
-var socket = io();
+var ioUrl = location.host + ":" + location.port + "/r-node";
+var socket = io(ioUrl, { path: '/r-node/socket.io' });
+socket.on("connected", function(msg) {
+  console.log(msg);
+});
 socket.on("message", function(msg) {
   $("<li>").append(msg).appendTo($("ul"));
 });
